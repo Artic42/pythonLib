81338f170347ba74c216adbe0f1ea62075d9182c
Artic42 <main@artic42.com>
2025-12-17 20:48:48 +0100

    Add log to file utils

--- articlib/articFileUtils.py
+++ articlib/articFileUtils.py
@@ -1,17 +1,25 @@
 import os
 import shutil
+import logging
+from articlib import logUtils as LU
+
+
+logObj = logging.getLogger()
 
 
 def deleteFile(path: str) -> None:
+    logObj.debug(f"Delete the file on path {path}")
     os.remove(path)
 
 
 def createFile(path: str) -> None:
+    logObj.debug(f"Create file on path {path}")
     file = open(path, "r")
     file.close()
 
 
 def copyFile(source: str, destination: str) -> None:
+    logObj.debug(f"Copy file from {source} to {destination}")
     shutil.copy(source, destination)
 
 
@@ -24,16 +32,21 @@ def getFilesInDirectory(path: str) -> list[str]:
 
 
 def deleteDirectory(path: str) -> None:
+    logObj.debug(f"Delete directory on {path}")
     os.rmdir(path)
 
 
 def createDirectory(path: str) -> None:
+    logObj.debug(f"Create directory on {path}")
     os.makedirs(path, exist_ok=True)
 
 
 class FileIO:
-    def __init__(self, path: str, readIt: bool = False) -> None:
+    def __init__(self, path: str, readIt: bool = False, log: bool = True) -> None:
+        self.log = log
         self.path = path
+        if self.log is True:
+            logObj.debug(f"Create FileIO object with path {self.path}")
         if readIt:
             self.readFile()
         else:
@@ -41,6 +54,9 @@ class FileIO:
 
     def readFile(self) -> None:
         filePointer = open(self.path, "r")
+        if self.log is True:
+            logObj.info(f"Read file in {self.path}")
+            LU.logFile(self.path, debugFlag=True)
         self.lines = filePointer.readlines()
         self.lines = [line[:-1] for line in self.lines]
         filePointer.close()
@@ -49,24 +65,41 @@ class FileIO:
         filePointer = open(self.path, "w")
         for line in self.lines:
             filePointer.write(line + "\n")
+        if self.log is True:
+            logObj.info(f"Write file in {self.path}")
+            LU.logFile(self.path, debugFlag=True)
+
         filePointer.close()
 
     def appendToFile(self) -> None:
         filePointer = open(self.path, "a")
         for line in self.lines:
             filePointer.write(line + "\n")
+        if self.log is True:
+            logObj.info(f"Append to file in {self.path}")
+            LU.logFile(self.path, debugFlag=True)
         filePointer.close()
 
     def addLine(self, line: str) -> None:
+        if self.log is True:
+            logObj.info(f"Addline: {line}")
         self.lines.append(line)
 
     def modifyLine(self, lineNumber: int, line: str) -> None:
+        if self.log is True:
+            logObj.info(f"Modify line {lineNumber} to {line}")
         self.lines[lineNumber] = line
 
     def removeLine(self, lineNumber: int) -> None:
+        if self.log is True:
+            logObj.info(f"Remove line {lineNumber}")
+            logObj.debug(f"Removed content is {self.lines[lineNumber]}")
         del self.lines[lineNumber]
 
     def removeLastLine(self) -> None:
+        if self.log is True:
+            logObj.info("Remove last line")
+            logObj.debug(f"Removed content is {self.lines[-1]}")
         del self.lines[-1]
 
     def findLine(self, text: str) -> list[str]:
@@ -77,6 +110,8 @@ class FileIO:
         return result
 
     def findAndReplace(self, find: str, replace: str) -> None:
+        if self.log is True:
+            logObj.info(f"Find {find} and replace it with {replace}")
         for i in range(len(self.lines)):
             self.lines[i] = self.lines[i].replace(find, replace)
 
